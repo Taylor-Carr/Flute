f17d8f303659f68340328e0d2c385180d9d51012
Taylor carr <taylormichaelcarr@yahoo.com>
2024-05-22 21:34:10 +0000

    added cart qauntity

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
 
 def cart_summary(request):
     return render(request, "cart_summary.html", {})
@@ -6,7 +9,21 @@ def cart_summary(request):
 
 
 def cart_add(request):
-    pass
+    cart = Cart(request)
+
+    if request.POST.get('action') == 'post':
+        #test for POST
+        product_id = int(request.POST.get('product_id'))
+        #look up product in DB
+        product = get_object_or_404(Product, id=product_id)
+        #save to session
+        cart.add(product=product)
+
+        cart_quantity = cart.__len__()
+        #return respone
+        #response = JsonResponse({'Product name: ': product.name})
+        response = JsonResponse({'qty': cart_quantity})
+        return response
 
 def cart_delete(request):
     pass
